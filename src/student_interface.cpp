de77d251bb27b3d4ad2e17f8dceff00fb83a1dc0
Enrico <enrico.berthod@studenti.unitn.it>
2019-11-08 10:42:51 +0100

    aggiornameto file iniziali

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -3,6 +3,11 @@
 
 #include <stdexcept>
 #include <sstream>
+#include <cstring>
+
+using namespace cv;
+using namespace std;
+
 namespace student {
 
  void loadImage(cv::Mat& img_out, const std::string& config_folder){  
@@ -10,7 +15,15 @@ namespace student {
  }
 
  void genericImageListener(const cv::Mat& img_in, std::string topic, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );
+	static int id = 0;
+	cv::imshow("test",img_in);
+	char key = (char)waitKey(30);
+	if (key=='s') {
+		imwrite(config_folder+"/img-saved"+to_string(id)+".jpg", img_in);
+		std::cout << config_folder+"/img-saved"+to_string(id)+".jpg"<< std::endl;
+		id++;
+	}
+	//throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );
   }
 
   bool extrinsicCalib(const cv::Mat& img_in, std::vector<cv::Point3f> object_points, const cv::Mat& camera_matrix, cv::Mat& rvec, cv::Mat& tvec, const std::string& config_folder){
@@ -24,13 +37,30 @@ namespace student {
 
   }
 
+  //-------------------------------------------------------------------------
+  //          FIND PLANE TRANSFORM
+  //-------------------------------------------------------------------------
   void findPlaneTransform(const cv::Mat& cam_matrix, const cv::Mat& rvec, const cv::Mat& tvec, const std::vector<cv::Point3f>& object_points_plane, cv::Mat& plane_transf, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );  
+    
+    cv::Mat image_points;
+
+    // project points
+    cv::projectPoints(object_points_plane, rvec, tvec, cam_matrix, cv::Mat(), image_points);
+
+    // find homography
+    std::vector<cv::Point2f> plane_points;
+    for (const auto pt: object_points_plane) {
+      plane_points.emplace_back(pt.x, pt.y);
+    }
+
+    plane_transf = cv::getPerspectiveTransform(image_points, plane_points);
   }
 
-
+  //-------------------------------------------------------------------------
+  //          UNWARP TRANSFORM
+  //-------------------------------------------------------------------------
   void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf, const double scale, const std::string& config_folder){
-    throw std::logic_error( "STUDENT FUNCTION NOT IMPLEMENTED" );   
+    cv::warpPerspective(img_in, img_out, transf, img_in.size());
   }
 
   bool processMap(const cv::Mat& img_in, const double scale, std::vector<Polygon>& obstacle_list, std::vector<std::pair<int,Polygon>>& victim_list, Polygon& gate, const std::string& config_folder){
