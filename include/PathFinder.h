f54b7b8c22a878949be00ba56c75181bc613ca5b
Mario Berti <mario.berti@studenti.unitn.it>
2020-08-18 14:39:10 +0200

    add new parameter in PathFinder function

--- include/PathFinder.h
+++ include/PathFinder.h
@@ -4,7 +4,8 @@
 #define PathFinder_H
 #include "DataStructure.h"
 #include "Voronoi.h"
+#include <opencv2/imgproc/imgproc.hpp>
 
-void PathFinder(VoronoiPoint, bool, VoronoiPoint, bool, VoronoiResults*, std::vector<VoronoiPoint> *rightPath, int netRadius);
+void PathFinder(VoronoiPoint, bool, VoronoiPoint, bool, VoronoiResults*, std::vector<VoronoiPoint> *, int, const std::vector<std::vector<cv::Point>>&);
 
 #endif
