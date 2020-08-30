b36c8cdb6f67fd771f5dbf83a16ce1ada31aa23c
BertiMario <mario.berti@studenti.unitn.it>
2020-08-30 15:08:35 +0200

    Update PlanPath_Interface.cpp

--- src/PlanPath_Interface.cpp
+++ src/PlanPath_Interface.cpp
@@ -384,6 +384,17 @@ void plan_Path123(const Polygon& borders, const std::vector<Polygon>& obstacle_l
 		printf(", %i\n",voronoiPaths.resultPoints[j].b);
 		collision_detection((double)voronoiPaths.resultPoints[j].a, (double)voronoiPaths.resultPoints[j].b, contours);
 	}
+	
+	for(int j=0;j<voronoiPaths.resultEdges.size();j++)
+	{
+		printf("p1: %i",voronoiPaths.resultEdges[j].idFirstNode);
+		printf(" ->(%i",voronoiPaths.resultEdges[j].p0.a);
+		printf(", %i)",voronoiPaths.resultPoints[j].p0.b);
+		printf(" - p2: %i",voronoiPaths.resultEdges[j].idSecondNode);
+		printf(" ->(%i",voronoiPaths.resultEdges[j].p1.a);
+		printf(", %i)",voronoiPaths.resultPoints[j].p1.b);
+		printf(" - lenght: %f",voronoiPaths.resultEdges[j].length);
+	}
 
 	//throw std::logic_error( "STOP" );
 	
